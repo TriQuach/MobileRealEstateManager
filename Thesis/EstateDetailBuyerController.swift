b26f5936b03e02c87661ad8c837b362efa71d8f5
Quách Minh Trí <qmtri@apcs.vn>
2017-04-08 00:58:46 +0700

    EstateDetailBuyerController

--- Thesis/EstateDetailBuyerController.swift
+++ Thesis/EstateDetailBuyerController.swift
@@ -9,16 +9,26 @@
 import Foundation
 import UIKit
 
-class EstateDetailBuyerController: UIViewController {
+class EstateDetailBuyerController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     
-   
+   let estates = ["house1", "house2","house3"]
     @IBOutlet weak var btnDate: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         //btnDate.ghostButton()
     }
-    
-    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return estates.count
+    }
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! EstateDetailBuyerControllerTableViewCell
+            cell.img.image = UIImage(named: estates[indexPath.row] + ".jpg")
+            cell.lblDescrip.text = "askdalskdamsklsadaskdalskdamsklsadaskdalskdamsklsadaskdalskdamsklsad"
+            return cell
+    }
     
 }
---
+++ Thesis/EstateDetailBuyerControllerTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  EstateDetailBuyerControllerTableViewCell.swift
+//  Thesis
+//
+//  Created by TriQuach on 4/7/17.
+//  Copyright © 2017 TriQuach. All rights reserved.
+//
+
+import UIKit
+
+class EstateDetailBuyerControllerTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var lblDescrip: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
